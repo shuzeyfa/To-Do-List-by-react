4464cf97515cd9464d8a3a15dd1de513bc4edfd8
Huzeyfa suleyman <shuzeyfa4@gmail.com>
2025-07-19 23:46:25 +0300

    Add files via upload

---
+++ main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import Index from '.'
+
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Index />
+  </React.StrictMode>,
+)
